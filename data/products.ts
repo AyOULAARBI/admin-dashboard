50f7454955c66d325357a355bef33388167ce204
Ayoub <ayoub6oulaarbi6@gmail.com>
2024-09-02 15:05:19 +0100

    test data

---
+++ data/products.ts
@@ -0,0 +1,24 @@
+import { Product } from "@/types/Product";
+const products: Product[] = [
+    { id: '1321', name: "iPhone 15 Pro Max", category: "Electronics", price: 1299.99, stock: 50 },
+    { id: '2321', name: "Levi's 501 Jeans", category: "Clothing", price: 79.99, stock: 200 },
+    { id: '3321', name: "Organic Avocado", category: "Food", price: 2.99, stock: 1000 },
+    { id: '4321', name: "Sony PlayStation 5", category: "Electronics", price: 499.99, stock: 10 },
+    { id: '5321', name: "Nike Air Max 1", category: "Clothing", price: 139.99, stock: 75 },
+    { id: '6321', name: "Coca-Cola 2-Liter Bottle", category: "Food", price: 1.99, stock: 500 },
+    { id: '7321', name: "Samsung Galaxy S24 Ultra", category: "Electronics", price: 1199.99, stock: 30 },
+    { id: '8321', name: "Adidas Ultraboost 23", category: "Clothing", price: 159.99, stock: 150 },
+    { id: '9321', name: "Apple Watch Series 9", category: "Electronics", price: 399.99, stock: 80 },
+    { id: '10321',  name: "Heinz Ketchup", category: "Food", price: 3.99, stock: 2000 },
+    { id: '11dfeqs', name: "MacBook Pro M2 Max", category: "Electronics", price: 2499.99, stock: 25 },
+    { id: '12dfeqs', name: "Ray-Ban Wayfarer Sunglasses", category: "Accessories", price: 189.99, stock: 100 },
+    { id: '13dfeqs', name: "Xbox Series X", category: "Electronics", price: 499.99, stock: 30 },
+    { id: '14dfeqs', name: "Canon EOS R5", category: "Photography", price: 3899.99, stock: 15 },
+    { id: '15dfeqs', name: "Bose QuietComfort 45 Headphones", category: "Audio", price: 299.99, stock: 70 },
+    { id: '16dfeqs', name: "Nintendo Switch OLED", category: "Gaming", price: 349.99, stock: 50 },
+    { id: '17dfeqs', name: "Beats Studio3 Wireless Headphones", category: "Audio", price: 249.99, stock: 120 },
+    { id: '18dfeqs', name: "Instant Pot Duo Crisp", category: "Kitchen Appliances", price: 149.99, stock: 80 },
+    { id: '19dfeqs', name: "Dyson V15 Detect", category: "Home Appliances", price: 699.99, stock: 40 },
+    { id: '20dfeqs', name: "GoPro Hero 11 Black", category: "Cameras", price: 399.99, stock: 60 },
+  ];
+export default products;
